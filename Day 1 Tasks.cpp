d3410dde2492a1096777427aeb1b54c67f29cc53
Meeran Ahmed Khan <meeran7khan@gmail.com>
2025-06-25 10:22:31 +0530

    Update Day 1 Tasks.cpp

--- Day 1 Tasks.cpp
+++ Day 1 Tasks.cpp
@@ -19,3 +19,24 @@ int main() {
 }
 
 //Task 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    float gpa;
+    
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your GPA: ";
+    cin >> gpa;
+    
+    cout << "\nYour Information:\n";
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "GPA: " << gpa << endl;
+    return 0;
+}
