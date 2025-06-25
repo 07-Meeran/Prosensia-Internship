409b3e4f98bffa24e920514fd4db3f542627faa2
Meeran Ahmed Khan <meeran7khan@gmail.com>
2025-06-25 10:21:08 +0530

    Update Day 1 Tasks.cpp

--- Day 1 Tasks.cpp
+++ Day 1 Tasks.cpp
@@ -1,6 +1,21 @@
+//Task 1
 #include<iostream>
 using namespace std;
 int main(){
 	cout<<"Hello World";
 return 0;
 }
+
+//Task2
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout <<"Enter your age:";
+    cin>>age;
+    cout<<"You entered:"<<age<<endl;
+    return 0;
+}
+
+//Task 3
